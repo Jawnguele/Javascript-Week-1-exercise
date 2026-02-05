0cd73c1420d76bded66d2120b0c01c3442b73e33
VINNIE 3 <kitamentary@Kitamentarys-MacBook-Pro.local>
2026-02-05 21:48:18 +0000

    Add conditionals practice files

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+// 3 - Array containing colleagues' names
+let colleagues = ["Kofi", "Collins", "Kayzier", "Isaac"];
+
+// 4️ - For loop to print each name
+for (let i = 0; i < colleagues.length; i++) {
+  console.log(colleagues[i]);
+}
+
+// 5️ - if, else if, else statement
+let age = 20;
+
+if (age < 18) {
+  console.log("You are under 18");
+} else if (age >= 18 && age < 60) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a senior");
+}
